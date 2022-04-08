5c8de31b4b9cded7d4a42af25f1a5c5ae8bc71e5
Jobayer-ak <jobayer.ak@gmail.com>
2022-04-08 14:28:02 +0600

    using github account as signin option

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import "./App.css";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "./firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
+
 function App() {
-  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
         const user = result.user;
         console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handlesignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(result.user);
       })
       .catch((error) => {
         console.log(error);
@@ -19,7 +53,19 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {/* Conditional Rendering */}
+      {user.email || user.uid ? (
+        <button onClick={handlesignOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      )}
+
+      <h2>{user.displayName}</h2>
+      <p>I know your email address: {user.email}</p>
+      <img src={user.photoURL} alt="User Image" />
     </div>
   );
 }
